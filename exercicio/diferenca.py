ef478b21b0cb8ceeaa41c12c6e7810f7077f318e
Aline Santana do Nascimento Pereira <apereira@informa.com.br>
2023-04-17 22:42:48 -0300

    Calculo diferença

---
+++ exercicio/diferenca.py
@@ -0,0 +1,7 @@
+#Ler 4 valores inteiros, calcular e mostra diferença do produto de A e B pelo produto de C e D
+A=int(input())
+B=int(input())
+C=int(input())
+D=int(input())
+DIFERENCA=(A*B)-(C*D)
+print("DIFERENCA =", DIFERENCA)
